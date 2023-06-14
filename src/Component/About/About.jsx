381c520123fe7dc3bb525e04f882aa70beb7ccf0
Azzi-Mo <alhawarymostafa@gmail.com>
2023-06-14 01:27:17 -0700

    edit

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -3,17 +3,19 @@
 // Next image optimization
 // import Image from "next/image";
 // import Technology from "@/components/Technology";
-import { AboutContainer, AboutmeH1, ContentBioP } from "./AboutStyle";
+import { AboutContainer, AboutmeH1, ContentBioP , UlContainerTech ,LiContainerTech } from "./AboutStyle";
 import Data from "./dataTech.json";
 function About() {
   return (
     <AboutContainer className="about" id="about">
       <div className="about__container">
-        <AboutmeH1 className="about__heading about__heading--section-heading">
-          Introduction
-        </AboutmeH1>
 
-        <div className="content">
+      
+      <div className="content">
+          <AboutmeH1 className="about__heading about__heading--section-heading">
+            Introduction
+          </AboutmeH1>
+      
           <div className="content__bio">
             <ContentBioP>
               Hello! My name is Mostafa El Hawary, I&apos;m{" "}
@@ -27,37 +29,25 @@ function About() {
               challenges and solving problems one project at a time. If you're
               looking for a full-stack software developer or a mobile app
               developer, you've come to the right place!
+              
+              <br />
+              <p>Here are a few technologies I&apos;ve been working with:</p>
             </ContentBioP>
 
-            <br />
-            <p>Here are a few technologies I&apos;ve been working with:</p>
-            <ul className="content__technologies">
+            <UlContainerTech className="content__technologies">
               {Data.map((technology) => (
-                <li className="technology" key={technology.id}>
-                  <div
-                    className="technology__icon"
-                    // style={{
-                    //   backgroundColor: color + "30",
-                    // }}
-                  >
-                    <img
-                      src={technology.img}
-                      alt={technology.tech}
-                      // width={22}
-                      // height={22}
-                    />
+                <LiContainerTech className="technology" key={technology.id}>
+                  <div className="technology__icon">
+                    <img src={technology.img} alt={technology.tech} />
                   </div>
                   <p className="technology__name">{technology.tech}</p>
-                </li>
+                </LiContainerTech>
               ))}
-            </ul>
+            </UlContainerTech>
           </div>
-          <div className="content__image">
-            {
-              // <Image src={logo} alt="Logo" fill placeholder="blur" priority />
-            }
-          </div>
+
         </div>
+
       </div>
     </AboutContainer>
   );
--- src/Component/About/AboutStyle.jsx
+++ src/Component/About/AboutStyle.jsx
@@ -1,13 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const AboutContainer = styled.section`
-
-
-height: 100vh;
+  height: 100vh;
   display: grid;
   align-items: center;
+  border: 1px solid red;
 
-`
+`;
 export const AboutmeH1 = styled.h1`
   margin-bottom: clamp(10px, 2vw, 20px);
   color: #64ffda;
@@ -15,12 +14,40 @@ export const AboutmeH1 = styled.h1`
   font-weight: 400;
   text-transform: none;
   font-size: clamp(1rem, 1.5vw, 4rem);
-`
+`;
+export const ContentBioContainer = styled.div`
 
+display: grid;
+
+`
 export const ContentBioP = styled.p`
+  font-size: clamp(1.1rem, 1.5vw, 1.5rem);
+  color: hsl(219, 14%, 63%);
+  padding: 20px 30% 20px 0;
+`;
+export const UlContainerTech = styled.ul`
+  width: 100%;
+  display: grid;
+  grid-template-columns: [] 50% [] 50%;
+`;
+export const LiContainerTech = styled.li`
+  width: 50%;
+  height: 80px;
+  list-style: none;
+  display: grid;
+  grid-template-columns: [] 5% [] 95%;
+  border: 1px solid red;
+  align-items: center;
+  ${css`
+    .technology__icon {
+      display: grid;
+      grid-template-columns: [] 20% [] 80%;
+      border: 1px solid red;
 
-font-size: clamp(1.1rem, 1.5vw, 1.5rem);
-color:hsl(219, 14%, 63%);
-padding: 20px 30% 20px 0 ;
-
-`
+      img {
+        width: 25px;
+        height: 25px;
+      }
+    }
+  `}
+`;
