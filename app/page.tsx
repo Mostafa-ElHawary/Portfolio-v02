9ca89ee5c5b4f709263a6f409577e17329d1d077
Azzi-Mo <alhwarymostafa@gmail.com>
2023-09-19 14:55:31 +0300

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Intro from '@/components/intro'
 export default function Home() {
   return (
-    <main className="page">
+    <main className="flex flex-col items-center px-4">
      <Intro/>
     </main>
   )
