78b30ae979815376759082f8650a3608dd0ab274
Azzi-Mo <alhwarymostafa@gmail.com>
2023-09-19 16:29:52 +0300

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import Intro from '@/components/intro'
+import Line from '@/components/line'
+import About from '@/components/about'
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
      <Intro/>
+     <Line/>
+     <About/>
     </main>
   )
 }
