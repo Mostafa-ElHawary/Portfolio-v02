190f38aa6a9df85d77a39d379bf082edbefcea49
Azzi-Mo <alhwarymostafa@gmail.com>
2023-09-19 13:59:29 +0300

    edit

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 export default function Intro() {
   return (
-    <section className="mb-28 max-w-[100rem] text-center sm:mb-0 scroll-mt-[100rem]">
+    <section className="mb-28  text-center sm:mb-0 scroll-mt-[100rem]">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
