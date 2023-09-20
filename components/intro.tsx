9dd66b063043c90a4a5b62fee1d71d7f2f244786
Azzi-Mo <alhwarymostafa@gmail.com>
2023-09-20 18:18:34 +0300

    edit

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,7 +62,8 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         I'm focusing on improving my skills and knowledge. You can see my work
-        on <span className="font-bold">GitHub </span>for more details. <span className="font-bold">Have a nice day!</span>{" "}
+        on <span className="font-bold">GitHub </span>for more details.{" "}
+        <span className="font-bold">Have a nice day!</span>{" "}
       </motion.h5>
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
@@ -79,22 +80,6 @@ export default function Intro() {
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/mostafa-el-hawary-542813248/"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
-
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/Mostafa-ElHawary"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/CV.pdf"
@@ -103,6 +88,24 @@ export default function Intro() {
           CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
+
+
+          {" "}
+          <a
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            href="https://www.linkedin.com/in/mostafa-el-hawary-542813248/"
+            target="_blank"
+          >
+            <BsLinkedin />
+          </a>
+          <a
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            href="https://github.com/Mostafa-ElHawary"
+            target="_blank"
+          >
+            <FaGithubSquare />
+          </a>
+
       </motion.div>
     </section>
   );
