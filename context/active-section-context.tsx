bb6936a000d3e90e0edf273156109c866613f47b
Azzi-Mo <alhwarymostafa@gmail.com>
2023-09-22 18:39:19 +0300

    edit

---
+++ context/active-section-context.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useContext, useState , createContext} from "react";
+import { links } from "@/lib/data";
+type linksOfSectionsType = (typeof links)[number]["name"];
+type ActiveSectionContextProviderProps = {
+  children: React.ReactNode;
+};
+type ActiveSectionContextType = {
+  activeSection: linksOfSectionsType;
+  setActiveSection: React.Dispatch<React.SetStateAction<linksOfSectionsType>>;
+};
+export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(
+  null
+);
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
+  const [activeSection, setActiveSection] =
+    useState<linksOfSectionsType>("Home");
+  return (
+    <ActiveSectionContext.Provider value={{ activeSection, setActiveSection }}>
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
+export function useActiveSectionContext(){
+    const context= useContext(ActiveSectionContext);
+
+    if(context === null){
+        throw new Error("No Context found") ;
+    }
+    return context
+}
