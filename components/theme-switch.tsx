290f282791dfd443bfbfb3869195502515b8821a
Azzi-Mo <alhwarymostafa@gmail.com>
2023-10-03 20:58:20 +0300

    edit

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -8,7 +8,7 @@ export default function ThemeSwitch() {
 
   return (
     <button
-      className="flex dark:border-none   dark:text-[#64ffda] justify-center items-center fixed bottom-5 right-5 h-[3rem] w-[3rem]  backdrop-blur-[0.1rem] border border-white border-opacity-40 shadow-2xl rounded-full hover:scale-[1.15] active:scale-105 transition-all "
+      className="flex dark:text-[#64ffda] justify-center items-center fixed bottom-5 right-5 h-[3rem] w-[3rem]     hover:scale-[1.15] active:scale-105 transition-all "
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
